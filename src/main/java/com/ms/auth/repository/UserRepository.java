990835c1c2c530320dcdb3a547360001383f218e
Emanuel Targino <emanuel.targino.700@ufrn.edu.br>
2022-09-01 21:49:56 -0300

    feat: inciando UserRepository

---
+++ src/main/java/com/ms/auth/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.ms.auth.repository;
+
+import com.ms.auth.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Integer> {
+
+    public Optional<UserModel> findByLogin(String login);
+
+}
