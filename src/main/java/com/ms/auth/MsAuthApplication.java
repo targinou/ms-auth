585fccb08d74516901f96c70310b0e095516ed9f
Emanuel Targino <emanuel.targino.700@ufrn.edu.br>
2022-09-02 09:32:59 -0300

    feat: validando senha

--- src/main/java/com/ms/auth/MsAuthApplication.java
+++ src/main/java/com/ms/auth/MsAuthApplication.java
@@ -3,6 +3,7 @@ package com.ms.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -13,9 +14,9 @@ public class MsAuthApplication {
 		SpringApplication.run(MsAuthApplication.class, args);
 	}
 
+	@Bean
 	public PasswordEncoder getPasswordEncoder(){
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		return encoder;
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- src/main/java/com/ms/auth/controler/UserController.java
+++ src/main/java/com/ms/auth/controler/UserController.java
@@ -2,11 +2,13 @@ package com.ms.auth.controler;
 
 import com.ms.auth.model.UserModel;
 import com.ms.auth.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
@@ -30,4 +32,23 @@ public class UserController {
         user.setPassword(encoder.encode(user.getPassword()));
         return ResponseEntity.ok(userService.save(user));
     }
+
+    @GetMapping("/validate-password")
+    public ResponseEntity<Boolean> validarSenha(@RequestParam String login,
+                                                @RequestParam String password) {
+
+        Optional<UserModel> optUser = userService.findByLogin(login);
+        if (optUser.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+        }
+
+        boolean valid = false;
+        valid = encoder.matches(password, optUser.get().getPassword());
+
+        HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+
+        return ResponseEntity.status(status).body(valid);
+
+    }
+
 }
