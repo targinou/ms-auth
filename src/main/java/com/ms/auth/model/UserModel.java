023a253cab6d1723407354b148632d3ce9628bd6
Emanuel Targino <emanuel.targino.700@ufrn.edu.br>
2022-09-01 21:49:43 -0300

    feat: inciando UserModel

---
+++ src/main/java/com/ms/auth/model/UserModel.java
@@ -0,0 +1,48 @@
+package com.ms.auth.model;
+
+import javax.persistence.*;
+
+@Entity
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true)
+    private String login;
+    private String password;
+
+    public UserModel() {
+    }
+
+    public UserModel(Integer id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
